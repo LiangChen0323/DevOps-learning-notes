024cf064b5c10e78b3cacdcf5996e818e9b0ccfa
Liang Chen <chenliang323@hotmail.com>
2020-03-10 10:01:54 +0000

    Update Python scripts 10/03/2020

---
+++ Python/UserInputFile.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.8
+
+
+def get_file_name(repromte=False):
+    if repromte:
+        print("Please entre a file name")
+
+    file_name = input("Destination file name:").strip()
+
+    return file_name or get_file_name(True)
+
+
+file_name = get_file_name()
+
+print(
+    f"Please enter your content. Entering an empty line will write the content to {file_name}:\n")
+
+with open(file_name, "w") as f:
+    eof = False
+    lines = []
+
+    while not eof:
+        line = input()
+        if line.strip():
+            lines.append(f"{line}\n")
+        else:
+            eof = True
+
+    f.writelines(lines)
+    print(f"lines written to {file_name}")
